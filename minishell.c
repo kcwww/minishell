6ba7bf9b470dbcd2199b10e18879ef1c544a98dc
kcwww <cwkim0321@gmail.com>
2023-04-26 11:22:43 +0900

    [feat] #4 - add readline

--- minishell.c
+++ minishell.c
@@ -6,13 +6,34 @@
 /*   By: kcw <kcw@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:32:07 by kcw               #+#    #+#             */
-/*   Updated: 2023/04/25 17:34:42 by kcw              ###   ########.fr       */
+/*   Updated: 2023/04/26 11:20:24 by kcw              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main()
+void	check_leaks(void)
 {
-	ft_printf("Hello, minishell!\n");
+	system("leaks minishell");
+}
+
+int	main(int argc, char **argv)
+{
+	char	*get_line;
+
+	atexit(check_leaks);
+	if (argc != 1)
+		return (0);
+	(void)argv;
+	while (1)
+	{
+		get_line = get_next_line(0);
+		if (get_line == NULL)
+		{
+			ft_printf("exit\n");
+			return (0);
+		}
+		ft_printf("%s", get_line);
+		free(get_line);
+	}
 }
--- minishell.h
+++ minishell.h
@@ -6,16 +6,16 @@
 /*   By: kcw <kcw@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:57:05 by kcw               #+#    #+#             */
-/*   Updated: 2023/04/25 17:44:44 by kcw              ###   ########.fr       */
+/*   Updated: 2023/04/26 11:17:39 by kcw              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "libft/libft.h"
-#include "parsing_src/parse.h"
-#include "builtins_src/bulitins.h"
-#include "redirections_src/redirections.h"
+# include "libft/libft.h"
+# include "parsing_src/parse.h"
+# include "builtins_src/bulitins.h"
+# include "redirections_src/redirections.h"
 
 #endif
