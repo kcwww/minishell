0a526271d12f8d7833c9e33245939334ff89567a
kcwww <cwkim0321@gmail.com>
2023-04-28 14:14:07 +0900

    [init] - initialize signal src

--- signal_src/mini_signal.h
+++ signal_src/mini_signal.h
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   mini_signal.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chanwoki <chanwoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/25 16:57:05 by kcw               #+#    #+#             */
-/*   Updated: 2023/04/28 14:12:27 by chanwoki         ###   ########.fr       */
+/*   Created: 2023/04/28 14:10:12 by chanwoki          #+#    #+#             */
+/*   Updated: 2023/04/28 14:10:44 by chanwoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef MINI_SIGNAL_H
+# define MINI_SIGNAL_H
 
-# include "libft/libft.h"
-# include "parsing_src/parse.h"
-# include "builtins_src/bulitins.h"
-# include "redirections_src/redirections.h"
-# include "error_src/error_shell.h"
-# include <signal.h>
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+
 
 #endif
