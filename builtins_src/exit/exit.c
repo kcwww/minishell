b97cf236eddc48454de2ad46e434933759d73a5f
dhkham <khamdong@gmail.com>
2023-05-02 21:59:51 +0900

    [feat] #11 - add exit.c function

--- builtins_src/exit/exit.c
+++ builtins_src/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 10:42:54 by dkham             #+#    #+#             */
-/*   Updated: 2023/05/02 21:56:20 by dkham            ###   ########.fr       */
+/*   Updated: 2023/05/02 21:59:10 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,32 @@
 
 void	cmd_exit(char **args, t_execute *execute)
 {
-	
+	int i;
+
+	i = 0;
+	if (args[1])
+	{
+		while (args[1][i])
+		{
+			if (ft_isdigit(args[1][i]) == 0)
+			{
+				ft_putstr_fd("exit\n", execute->fd_out);
+				ft_putstr_fd("minishell: exit: ", execute->fd_out);
+				ft_putstr_fd(args[1], execute->fd_out);
+				ft_putstr_fd(": numeric argument required\n", execute->fd_out);
+				exit(255);
+			}
+			i++;
+		}
+		if (args[2])
+		{
+			ft_putstr_fd("exit\n", execute->fd_out);
+			ft_putstr_fd("minishell: exit: too many arguments\n", execute->fd_out);
+			exit(1);
+		}
+		else
+			exit(ft_atoi(args[1]));
+	}
+	else
+		exit(0);
 }
