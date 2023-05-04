7fdc3273e456fc360fb44e80cdfa2d94ec00b5e8
dhkham <khamdong@gmail.com>
2023-05-04 19:45:57 +0900

    [feat] #14 add $ option to echo.c

--- builtins_src/bulitins.h
+++ builtins_src/bulitins.h
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:36:14 by kcw               #+#    #+#             */
-/*   Updated: 2023/05/03 21:44:19 by dkham            ###   ########.fr       */
+/*   Updated: 2023/05/04 19:40:00 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void	unset(char **args, t_execute *execute);
 void	env(t_execute *execute);
 void	cmd_exit(char **args, t_execute *execute);
 int		only_n_after_dash(const char *str);
-
+char	*expand_env_var(const char *str);
+void	print_args(char **args, int i, int n_flag, t_execute *execute);
+int		handle_n_flag(char **args, int *i);
 
 #endif
--- builtins_src/echo/echo.c
+++ builtins_src/echo/echo.c
@@ -6,11 +6,13 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 18:09:50 by dkham             #+#    #+#             */
-/*   Updated: 2023/05/03 22:00:38 by dkham            ###   ########.fr       */
+/*   Updated: 2023/05/04 19:38:39 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+// echo $a option 구현해야 함
 
 int	only_n_after_dash(const char *str)
 {
@@ -26,30 +28,95 @@ int	only_n_after_dash(const char *str)
 	return (1);
 }
 
-void	echo(char **args, t_execute *execute)
+char	*expand_env_var(const char *str)
 {
-	int	i;
+	char	*value;
+
+	if (str[0] == '$')
+	{
+		value = getenv(str + 1);
+		if (value)
+			return (value);
+	}
+	return (NULL);
+}
+
+// void	echo(char **args, t_execute *execute)
+// {
+// 	int		i;
+// 	int		n_flag;
+// 	char	*expanded;
+
+// 	i = 1;
+// 	n_flag = 0;
+// 	while (args[i] && ft_strncmp(args[i], "-n", 2) == 0)
+// 	{
+// 		if (only_n_after_dash(args[i]))
+// 		{
+// 			n_flag = 1;
+// 			i++;
+// 		}
+// 		else
+// 			break ;
+// 	}
+// 	while (args[i])
+// 	{
+// 		expanded = expand_env_var(args[i]);
+// 		if (expanded)
+// 			ft_putstr_fd(expanded, execute->fd_out);
+// 		else
+// 			ft_putstr_fd(args[i], execute->fd_out);
+// 		if (args[i + 1])
+// 			ft_putstr_fd(" ", execute->fd_out);
+// 		i++;
+// 	}
+// 	if (n_flag == 0)
+// 		ft_putstr_fd("\n", execute->fd_out);
+// }
+
+int	handle_n_flag(char **args, int *i)
+{
 	int	n_flag;
 
-	i = 1;
 	n_flag = 0;
-	while (args[i] && ft_strncmp(args[i], "-n", 2) == 0)
+	while (args[*i] && ft_strncmp(args[*i], "-n", 2) == 0)
 	{
-		if (only_n_after_dash(args[i]))
+		if (only_n_after_dash(args[*i]))
 		{
 			n_flag = 1;
-			i++;
+			(*i)++;
 		}
 		else
 			break ;
 	}
+	return (n_flag);
+}
+
+void	print_args(char **args, int i, int n_flag, t_execute *execute)
+{
+	char	*expanded;
+
 	while (args[i])
 	{
-		ft_putstr_fd(args[i], execute->fd_out);
+		expanded = expand_env_var(args[i]);
+		if (expanded)
+			ft_putstr_fd(expanded, execute->fd_out);
+		else
+			ft_putstr_fd(args[i], execute->fd_out);
 		if (args[i + 1])
 			ft_putstr_fd(" ", execute->fd_out);
 		i++;
 	}
 	if (n_flag == 0)
 		ft_putstr_fd("\n", execute->fd_out);
+}
+
+void	echo(char **args, t_execute *execute)
+{
+	int	i;
+	int	n_flag;
+
+	i = 1;
+	n_flag = handle_n_flag(args, &i);
+	print_args(args, i, n_flag, execute);
 }
