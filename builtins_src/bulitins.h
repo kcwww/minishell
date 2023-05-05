1c59b750ef7857704b840cb3a86b7e7ebfdc89ac
dhkham <khamdong@gmail.com>
2023-05-05 12:54:46 +0900

    [feat] #15 add envp functions to minishell.c

--- builtins_src/bulitins.h
+++ builtins_src/bulitins.h
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:36:14 by kcw               #+#    #+#             */
-/*   Updated: 2023/05/04 19:40:00 by dkham            ###   ########.fr       */
+/*   Updated: 2023/05/05 11:52:40 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,11 @@ int		only_n_after_dash(const char *str);
 char	*expand_env_var(const char *str);
 void	print_args(char **args, int i, int n_flag, t_execute *execute);
 int		handle_n_flag(char **args, int *i);
+char	*get_parent_dir(void);
+void	handle_special_cases(char **dir, char **last_dir);
+char	**copy_envp(char **envp);
+void	free_env(char **envp);
+
+
 
 #endif
--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:32:07 by kcw               #+#    #+#             */
-/*   Updated: 2023/05/02 21:04:33 by dkham            ###   ########.fr       */
+/*   Updated: 2023/05/05 12:51:20 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@
 // 	system("leaks minishell");
 // }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	char	*get_line;
+	char		*get_line;
+	char		**args;
+	t_execute	execute;
+	char		**env;
 
-	//atexit(check_leaks);
 	if (argc != 1)
 		return (0);
 	(void)argv;
+	execute.env = copy_envp(envp);
 	while (1)
 	{
 		get_line = readline("minishell $ ");
@@ -33,17 +36,47 @@ int	main(int argc, char **argv)
 			error_str();
 			return (0);
 		}
-		// split line
-		char **args = ft_split(get_line, ' ');
-		// set struct
-		t_execute execute;
+		args = ft_split(get_line, ' ');
 		execute.fd_out = 1;
-		// run builtin function
 		builtin(args, &execute);
-		// add_history
 		add_history(get_line);
-		//ft_printf("%s\n", get_line);
 		free(get_line);
 	}
+	free_env(env);
 	return (0);
+}
+
+char	**copy_envp(char **envp)
+{
+	int		count;
+	int		i;
+	char	**new_envp;
+
+	count = 0;
+	i = 0;
+	while (envp[count])
+		count++;
+	new_envp = (char **)malloc((count + 1) * sizeof(char *));
+	if (!new_envp)
+		return (NULL);
+	while (i < count)
+	{
+		new_envp[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	new_envp[count] = NULL;
+	return (new_envp);
+}
+
+void	free_env(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		free(envp[i]);
+		i++;
+	}
+	free(envp);
 }
