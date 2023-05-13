939fd7ee3aa210536a173463db34bd78c1cd5e63
dhkham <khamdong@gmail.com>
2023-05-13 12:15:44 +0900

    [feat] add heredoc part

--- builtins_src/builtin.c
+++ builtins_src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 20:55:14 by dkham             #+#    #+#             */
-/*   Updated: 2023/05/13 10:39:29 by dkham            ###   ########.fr       */
+/*   Updated: 2023/05/13 12:15:12 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,48 +32,114 @@
 // 		ft_putendl_fd("minishell: command not found", 1);
 // }
 
-void	builtin(t_execute *execute)
+char	*check_access(t_execute *my_shell, char *cmd)
 {
-	char	**word;
+	int		i;
+	char	*path;
+	char	**paths;
 
-	word = execute->head->simple_cmd->word;
-	if (ft_strncmp(word[0], "echo", 5) == 0)
-		echo(execute);
-	else if (ft_strncmp(word[0], "pwd", 4) == 0)
-		pwd();
-	else if (ft_strncmp(word[0], "cd", 3) == 0)
-		cd(execute);
-	else if (ft_strncmp(word[0], "export", 7) == 0)
-		export(execute);
-	else if (ft_strncmp(word[0], "unset", 6) == 0)
-		unset(execute);
-	else if (ft_strncmp(word[0], "env", 4) == 0)
-		env(execute);
-	else if (ft_strncmp(word[0], "exit", 5) == 0)
-		cmd_exit(execute);
-	else
+	// If the command is already an absolute path, return it directly.
+	if (cmd[0] == '/')
 	{
-		printf("minishell: command not found: %s\n", word[0]);
-		exit(EXIT_FAILURE);
+		if (access(cmd, X_OK) == 0) // if the file exists and is executable
+			return (ft_strdup(cmd));
+		else
+			return (NULL);
 	}
+
+	paths = get_paths_from_env(my_shell->env);
+	i = 0;
+	while (paths && paths[i]) // iterate through the path list to find the file
+	{
+		path = ft_strjoin(paths[i], "/"); // append slash
+		char *full_path_to_cmd = ft_strjoin(path, cmd);
+		free(path);
+		if (access(full_path_to_cmd, X_OK) == 0) // if the file exists and is executable, break the loop
+		{
+			free_2d_array(paths);
+			return (full_path_to_cmd);
+		}
+		free(full_path_to_cmd);
+		i++;
+	}
+	free_2d_array(paths);
+	return (NULL);
+}
+
+// A helper function to count the number of segments separated by ':'
+int count_paths(char *str)
+{
+    int count = 0;
+    for (int i = 0; str[i]; i++)
+        if (str[i] == ':')
+            count++;
+    return count + 1; // +1 for the extra path before the first :
+}
+
+// A helper function to get a single path segment
+char *get_path(char **str)
+{
+    char *start = *str;
+    while (**str && **str != ':')
+        (*str)++;
+    char *path = ft_substr(start, 0, *str - start);
+    if (**str)
+        (*str)++; // Skip the ':' character
+    return path;
+}
+
+char **get_paths_from_env(t_env *env)
+{
+	char *path_var = NULL;
+	t_env *current = env;
+
+	// Iterate through the linked list to find the PATH variable
+	while (current != NULL)
+	{
+		if (ft_strncmp(current->key, "PATH", ft_strlen("PATH")) == 0)
+		{
+			path_var = current->value;
+			break;
+		}
+		current = current->next;
+	}
+
+	// If PATH variable is not found, return NULL
+	if (!path_var)
+		return NULL;
+
+	// Count the number of paths in the PATH variable
+	int path_count = count_paths(path_var);
+
+	// Allocate memory for the paths array
+	char **paths = (char **)malloc(sizeof(char *) * (path_count + 1)); // +1 for the NULL at the end
+
+	// Get the separate paths
+	int i = 0;
+	while (i < path_count)
+	{
+		paths[i] = get_path(&path_var);
+		i++;
+	}
+	paths[path_count] = NULL;
+
+	return (paths); // Added parentheses in the return statement
 }
 
 void	execute(t_execute *my_shell)
 {
-	t_pipes *head;
-	pid_t pid;
-	int fd[2];
-	t_cmd *cmd;
+	t_pipes	*head;
+	pid_t	pid;
+	int		fd[2];
+	t_cmd	*cmd;
+	char	*path_to_cmd;
 
 	head = my_shell->head;
 	while (head)
 	{
 		cmd = head->simple_cmd;
-
 		if (!head->next && is_builtin(cmd->word[0]))  // 파이프가 없고 빌트인 명령어인 경우
-		{
 			builtin(my_shell);  // 빌트인 명령어 바로 실행
-		}
 		else  // 파이프가 있거나 빌트인 명령어가 아닌 경우
 		{
 			if (head->next && pipe(fd) < 0)  // 파이프가 있는 경우 파이프 생성
@@ -106,9 +172,19 @@ void	execute(t_execute *my_shell)
 					builtin(my_shell);  // 빌트인 명령어 바로 실행
 				else  // 빌트인 명령어가 아닌 경우
 				{
-					execve(cmd->word[0], cmd->word, NULL);
-					perror("execve error");
-					exit(EXIT_FAILURE);
+					path_to_cmd = check_access(my_shell, cmd->word[0]);
+					if (path_to_cmd != NULL)
+					{
+						execve(path_to_cmd, cmd->word, NULL);
+						free(path_to_cmd);
+						perror("execve error");
+						exit(EXIT_FAILURE);
+					}
+					else
+					{
+						fprintf(stderr, "minishell: %s: command not found\n", cmd->word[0]);
+						exit(EXIT_FAILURE);
+					}
 				}
 			}
 			else  // 부모 프로세스
@@ -122,6 +198,31 @@ void	execute(t_execute *my_shell)
 	}
 }
 
+void	builtin(t_execute *execute)
+{
+	char	**word;
+
+	word = execute->head->simple_cmd->word;
+	if (ft_strncmp(word[0], "echo", 5) == 0)
+		echo(execute);
+	else if (ft_strncmp(word[0], "pwd", 4) == 0)
+		pwd();
+	else if (ft_strncmp(word[0], "cd", 3) == 0)
+		cd(execute);
+	else if (ft_strncmp(word[0], "export", 7) == 0)
+		export(execute);
+	else if (ft_strncmp(word[0], "unset", 6) == 0)
+		unset(execute);
+	else if (ft_strncmp(word[0], "env", 4) == 0)
+		env(execute);
+	else if (ft_strncmp(word[0], "exit", 5) == 0)
+		cmd_exit(execute);
+	else
+	{
+		printf("minishell: command not found: %s\n", word[0]);
+		exit(EXIT_FAILURE);
+	}
+}
 
 int	is_builtin(char *cmd)
 {
@@ -145,19 +246,18 @@ int	is_builtin(char *cmd)
 	return (0);  // 빌트인 명령어가 아닐 경우 0을 반환
 }
 
-
-
 void	handle_redirections(t_cmd *cmd)
 {
-	int	i;
-	int	fd;
+	int		i;
+	int		fd;
+	char	*line;
 
 	i = 0;
 	while (cmd->redirection[i])
 	{
 		if (ft_strncmp(cmd->redirection[i], "<", 2) == 0)
 		{
-			fd = open(cmd->redir_value[i], O_RDONLY); // 입력 리다이렉션
+			fd = open(cmd->redir_value[i], O_RDONLY, 0644); // 입력 리다이렉션
 			if (fd < 0)
 			{
 				perror("open error");
@@ -188,8 +288,39 @@ void	handle_redirections(t_cmd *cmd)
 			dup2(fd, STDOUT_FILENO);
 			close(fd);
 		}
-		// 여기서 "<<" 히어 도큐먼트 리다이렉션을 처리하려면 추가적인 로직이 필요합니다.
-		// 일반적으로 임시 파일을 생성하고 거기에 입력을 받은 뒤, 그 파일을 표준 입력으로 리다이렉트합니다.
+		else if (ft_strncmp(cmd->redirection[i], "<<", 3) == 0) // heredoc
+		{
+			fd = open("temp.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644); // Open a temporary file for here-document redirection
+			if (fd < 0)
+			{
+				perror("open error");
+				exit(EXIT_FAILURE);
+			}
+			while (1)
+			{
+				line = readline("> "); // Read the input
+				if (strcmp(line, cmd->redir_value[i]) == 0) // If the limiter is found, break out of the loop
+					break;
+				write(fd, line, strlen(line)); // Write the input into the temporary file
+				write(fd, "\n", 1); // Write a newline character
+				free(line); // Don't forget to free the allocated memory
+			}
+			free(line); // Don't forget to free the allocated memory
+			close(fd); // Close the temporary file
+			fd = open("temp.txt", O_RDONLY, 0644); // Re-open the temporary file for reading
+			if (fd < 0)
+			{
+				perror("open error");
+				exit(EXIT_FAILURE);
+			}
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+			if (remove("temp.txt") != 0) // Delete the temporary file
+			{
+				perror("Error deleting temporary file");
+				exit(EXIT_FAILURE);
+			}
+		}
 		i++;
 	}
 }
