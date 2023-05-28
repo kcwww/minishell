2275f879e2308705983546d30d95cda7c0004845
kcwww <cwkim0321@gmail.com>
2023-05-28 15:46:51 +0900

    [feat] - skip newline & onlywhite space

--- include/parse.h
+++ include/parse.h
@@ -6,7 +6,7 @@
 /*   By: chanwoki <chanwoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:29:49 by kcw               #+#    #+#             */
-/*   Updated: 2023/05/13 10:30:36 by chanwoki         ###   ########.fr       */
+/*   Updated: 2023/05/28 15:45:08 by chanwoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 int		check_quotation(char *str);
 int		check_double(char *str, int i, char mark);
+int		check_whitespace(char *str);
 
 #endif
--- minishell.c
+++ minishell.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
+/*   By: chanwoki <chanwoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 20:57:40 by dkham             #+#    #+#             */
-/*   Updated: 2023/05/26 20:59:44 by dkham            ###   ########.fr       */
+/*   Updated: 2023/05/28 15:45:59 by chanwoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void	check_leaks(void)
-// {
-// 	system("leaks minishell");
-// }
+void	check_leaks(void)
+{
+	system("leaks minishell");
+}
 
 int	main(int argc, char **argv, char **env)
 {
 	char	*get_line;
 	t_shell	my_shell;
 
-	//atexit(check_leaks);
+	atexit(check_leaks);
 	if (argc != 1)
 		return (0);
 	(void)argv;
@@ -30,15 +30,18 @@ int	main(int argc, char **argv, char **env)
 	while (1)
 	{
 		get_line = readline("minishell $ ");
+		if (ft_strcmp("", get_line) == 0)
+		{
+			free(get_line);
+			continue ;
+		}
 		if (get_line == NULL)
 		{
 			printf("exit\n");
 			free_env(my_shell.env);
 			return (0);
 		}
-
-
-		if (check_quotation(get_line))
+		if (check_quotation(get_line) && check_whitespace(get_line))
 		{
 			parsing_start(get_line, &my_shell);
 			if (my_shell.error == 1)
