b30781594ebf4d3b1f6d231d2f6ce08fff224ff2
dhkham <khamdong@gmail.com>
2023-06-07 20:13:54 +0900

    [refactor] remove unnecessary lines in make_hd

--- heredoc_redir.c
+++ heredoc_redir.c
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 19:46:57 by dkham             #+#    #+#             */
-/*   Updated: 2023/06/07 20:10:00 by dkham            ###   ########.fr       */
+/*   Updated: 2023/06/07 20:13:27 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,15 +55,11 @@ void	make_hd(t_shell *my_shell, char *filename, char *end_str)
 	{
 		line = readline("> ");
 		if (ft_strcmp(line, end_str) == 0)
-		{
-			free(line);
 			break ;
-		}
 		else if (line == NULL)
 		{
 			dup2(save_stdin, 0);
 			init_signal(my_shell);
-			free(line);
 			break ;
 		}
 		ft_putendl_fd(line, fd);
